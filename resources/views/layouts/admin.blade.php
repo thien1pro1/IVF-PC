6a9275bbf0d721621bcad118837c027ff298b1a3
hoanghoc01 <hocb1805759@student.ctu.edu.vn>
2022-11-23 14:08:44 +0700

    a

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -25,9 +25,9 @@ filebrowserWindowHeight : 500
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="{{asset('backend/img/down-arrow')}}">
-  
+  <link rel="icon" type="image/png" href="{{asset('backend/img/logo.png')}}">
   <title>
-    D
+    IVF PC
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
