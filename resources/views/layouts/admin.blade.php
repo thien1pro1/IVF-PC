9430ece1ce92167df6a6f0c49da41cb655173c71
Thiên Nguyễn <thien1pro1@gmail.com>
2022-12-12 15:57:49 +0700

    xong

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -538,9 +538,7 @@ filebrowserWindowHeight : 500
     </div>
   </main>
   <div class="fixed-plugin">
-    <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
-      <i class="fa fa-cog py-2"> </i>
-    </a>
+
     <div class="card shadow-lg ">
       <div class="card-header pb-0 pt-3 ">
       
